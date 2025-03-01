b1764d1e0eb16601dca592bc1a266f03ef2e37ab
curlyYems <y.feliz1296@gmail.com>
2025-03-01 11:50:13 -0500

    dropdown/nav styles

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,10 +1,5 @@
-import React, { Component } from 'react';
-import {
-    BrowserRouter as Router,
-    Routes,
-    Route,
-    Link,
-} from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 import '../styles/NavBar.css';
 import '../src/App.css';
@@ -13,6 +8,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import Button from 'react-bootstrap/Button';
 
 import Home from '../pages/Home';
 import RegularCleaning from '../Pages/Residential/RegularCleaning';
@@ -21,84 +17,67 @@ import MoveCleaning from '../Pages/Residential/MoveCleaning';
 import PostConstruction from '../Pages/Residential/PostConstruction';
 import Commercial from '../pages/Commercial';
 import EcoFriendly from '../pages/Eco-Friendly';
-import Career from '../Pages/Career'
+import Career from '../Pages/Career';
 import CallAction from '../pages/CallAction';
-import logo from '../../assets/logo.png'; // Adjust path based on file location
-
+import logo from '../src/assets/logo.png'; 
 
 const Navigation = () => {
   return (
     <Router>
-    <Navbar fixed="top" collapseOnSelect expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="/">
-          <img alt="ruxcleaninglogo" src={logo} width="30" height="30" className="d-inline-block align-top" />
+      <Navbar fixed="top" collapseOnSelect expand="lg" className="bg-body-tertiary">
+        {/* <Container> */}
+          {/* ✅ Fix Logo & Brand Name Alignment */}
+          <Navbar.Brand href="/" className="d-flex align-items-center">
+            <img 
+              alt="ruxcleaninglogo" 
+              src={logo} 
+              width="150" // Adjust width
+              height="150" // Set height explicitly
+              className="d-inline-block align-top" 
+            />
+            <span className="brand-name">RuxCleaning</span> {/* Styled in CSS */}
+          </Navbar.Brand>
 
-          {' '}RuxCleaning</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            {/* ✅ Ensure NavLinks Take Full Space */}
+            <Nav className="ms-auto w-100 d-flex justify-content-around">
+              <Nav.Link as={Link} to="/">Home</Nav.Link>
 
-            <Nav.Link as={Link} to="/">Home</Nav.Link>
-
-            <NavDropdown title="Residential" id="collapsible-nav-dropdown">
-              <NavDropdown.Item as={Link} to="/regularCleaning" >Regular Cleaning</NavDropdown.Item>
-
-              <NavDropdown.Item as={Link} to="/deepCleaning">
-                Deep Cleaning
-              </NavDropdown.Item>
-
-              <NavDropdown.Item as={Link} to="/moveCleaning">Move in/out Cleaning</NavDropdown.Item>
-
-              <NavDropdown.Item as={Link} to="/postConstructionCleaning">
-                Post Construction Cleaning
-              </NavDropdown.Item>
+              <NavDropdown title="Residential" id="collapsible-nav-dropdown" className='custom-dropdown'>
+                
+                <NavDropdown.Item as={Link} to="/regularCleaning">Regular Cleaning</NavDropdown.Item>
+                <NavDropdown.Item as={Link} to="/deepCleaning">Deep Cleaning</NavDropdown.Item>
+                <NavDropdown.Item as={Link} to="/moveCleaning">Move in/out Cleaning</NavDropdown.Item>
+                <NavDropdown.Item as={Link} to="/postConstructionCleaning">Post Construction Cleaning</NavDropdown.Item>
+                
               </NavDropdown>
 
               <Nav.Link as={Link} to="/commercial">Commercial</Nav.Link>
-
               <Nav.Link as={Link} to="/ecoFriendly">Eco Friendly</Nav.Link>
-
               <Nav.Link as={Link} to="/career">Career</Nav.Link>
+              <Button className="callBtn" variant="secondary" as={Link} to="/callAction">Call Now 777-777-7777</Button>
+            </Nav>
+          </Navbar.Collapse>
+        {/* </Container> */}
+      </Navbar>
 
-              <Nav.Link as={Link} to="/callAction">Call Action</Nav.Link>
-          </Nav>
-          {/* <Nav>
-            <Nav.Link href="#deets">More deets</Nav.Link>
-            <Nav.Link eventKey={2} href="#memes">
-              Dank memes
-            </Nav.Link>
-          </Nav> */}
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-    <div>
+      <div>
         <Routes>
-            <Route path="/" element={<Home />}> </Route>
-
-            <Route path="/regularCleaning" element={<RegularCleaning />}> </Route>
-
-            <Route path="/deepCleaning" element={<DeepCleaning />}> </Route>
-
-            <Route path="/moveCleaning" element={<MoveCleaning />}> </Route>
-
-            <Route path="/postConstructionCleaning" element={<PostConstruction />}> </Route>
-
-            <Route path="/commercial" element={<Commercial />}> </Route>
-
-            <Route path="/ecoFriendly" element={<EcoFriendly />}> </Route>
-
-            <Route path="/career" element={<Career />}> </Route>
-
-            <Route path="/callAction" element={<CallAction />}> </Route>
-
-
-
-
+          <Route path="/" element={<Home />} />
+          <Route path="/regularCleaning" element={<RegularCleaning />} />
+          <Route path="/deepCleaning" element={<DeepCleaning />} />
+          <Route path="/moveCleaning" element={<MoveCleaning />} />
+          <Route path="/postConstructionCleaning" element={<PostConstruction />} />
+          <Route path="/commercial" element={<Commercial />} />
+          <Route path="/ecoFriendly" element={<EcoFriendly />} />
+          <Route path="/career" element={<Career />} />
+          <Route path="/callAction" element={<CallAction />} />
         </Routes>
-    </div>
-
+      </div>
     </Router>
   );
-}
+};
+
 export default Navigation;
+
